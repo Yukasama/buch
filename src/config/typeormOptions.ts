333c46ded0e4fbaf8b38e4b87377d3a71cfb817a
Yukasama <53038817+Yukasama@users.noreply.github.com>
2024-05-28 18:39:27 +0200

    Remove ngrok ports

--- src/config/typeormOptions.ts
+++ src/config/typeormOptions.ts
@@ -68,7 +68,7 @@ switch (dbType) {
     dataSourceOptions = {
       type: 'postgres',
       host,
-      port: 14_652,
+      port: 5432,
       username,
       password: pass,
       database,
@@ -168,7 +168,7 @@ if (dbType === 'postgres') {
   adminDataSourceOptionsTemp = {
     type: 'postgres',
     host,
-    port: 14_652,
+    port: 5432,
     username: 'postgres',
     password: passAdmin,
     database,
